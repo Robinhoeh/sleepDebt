b55739ea5c84bafbe0b284cf235804fa02bc4610
Robin Watson <drumsrobin@gmail.com>
2017-09-30 13:58:17 -0400

    updating js file

--- src/sleepdebt.js
+++ src/sleepdebt.js
@@ -59,25 +59,74 @@ To get the total sleep hours that the user actually slept, declare a new functio
 
 */
 
-const getActualSleepHours = () => {
+const getActualSleepHours = () => 
 	getSleepHours('monday') + getSleepHours('tuesday') + getSleepHours('wednesday') +
 	getSleepHours('thursday') +getSleepHours('friday') +getSleepHours('saturday') +
 	getSleepHours('sunday');
-}
 
+//no brackets IMPLIES return  = implicit return
 
 //Inside the getActualSleepHours() function, call the getSleepHours() function for 
 //each day of the week. Add them all together and return the result using the arrow function syntax and implicit return.
 
 
-   
+const getIdealSleepHours = () => {
+
+const IdealSleepHours = 8;//8 hours is reco by the doc
+	return IdealSleepHours * 7;//number of days per week
+}
+
+
+
+//To get the ideal sleep hours that the user prefers, declare a function named getIdealSleepHours() with no parameters.
+//Inside the function, declare a variable named idealHours and set its value to your ideal hours per night. 
+//Then return the idealHours multiplied by 7.
+//You'll want to multiply by 7 to get the total hours you prefer per week.
+
+
+
+
+const calculateSleepDebt = () => {
+	const actualSleepHours = getActualSleepHours();
+	const IdealSleepHours = getIdealSleepHours();
+
+	if (actualSleepHours === idealSleepHours) {
+		console.log('You have the perfect amount of sleep!');
+	} else if (actualSleepHours > idealSleepHours) {
+		console.log('You got too much sleep maybe');
+	} else if (actualSleepHours < idealSleepHours) {
+		console.log(`You said you wanted ${idealSleepHours} hours per week. You are actually getting 
+			${idealSleepHours - actualSleepHours} hours of sleep per week!`);
+	}
+}
+
+console.log( calculateSleepDebt() );
+
+
+
+//Now that you can get the actual sleep hours and the ideal sleep hours, it's time to calculate sleep debt.
+
+//Declare a function named calculateSleepDebt() with no parameters.
+
+//Inside of its block, create a variable named actualSleepHours set equal to the getActualSleepHours() function call.
+
+//Then, create another variable named idealSleepHours, set equal to the getIdealSleepHours() function call.
 
 
 
 
 
+//Now that you have actualSleepHours and idealSleepHours, you can write a few if/else 
+// statements to output the result to the user. The function should fulfill this logic:
+
+// If actual sleep equals ideal sleep, log to the console that the user got the perfect amount of sleep.
+// If the actual sleep is greater than the ideal sleep, log to the console that the user got more sleep than needed.
+// If the actual sleep is less than the ideal sleep, log to the console that the user should get some rest.
 
 
+
+// To make this calculator more helpful, log the amount of hours the user is over or under their ideal 
+// sleep in the calculateSleepDebt() function's output.
 
 
 
